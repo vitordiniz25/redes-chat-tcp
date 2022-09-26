2bedec6e82b185a49085967f853b8f861d7c18e5
Natanael Filho <filhonatanael01@gmail.com>
2022-09-26 01:03:42 -0300

    att to have a better log

--- servidor.c
+++ servidor.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
 
+#define MAX_SALAS 100
+#define MAX_STR_SIZE 256
 
+#define true 1
+#define false 0
 
-fd_set master, read_fds;
-struct sockaddr_in myaddr, remoteaddr;
-int fdmax, newfd, nbytes, yes=1, addrlen;
-char buf[256];
+typedef int bool;
 
 typedef struct {
     int cliente_sd;
-    char nome[256];
+    char nome[MAX_STR_SIZE];
     int ativo;
 } cliente;
 
@@ -25,12 +26,66 @@ typedef struct {
     cliente *clientes;
 } sala;
 
-sala salas[100];
+fd_set master, read_fds;
+struct sockaddr_in myaddr, remoteaddr;
+int fdmax, newfd, nbytes, yes=1, addrlen;
+char buf[MAX_STR_SIZE];
+sala salas[MAX_SALAS];
 
+void inicia_servidor () {
+    // Inicializacao do servidor, zerando valores de todas as salas
+    for (int i = 0; i < MAX_SALAS; i++) {
+        FD_ZERO(&salas[i].sala_fd);
+        salas[i].limite = 0;
+        salas[i].quantidade = 0;
+        salas[i].ativo = false;
+    }
+}
+
+void sair_da_sala (int sd, int sala_id, int cliente_id, int retirar_master) {
+    printf("Sala %d: arquivo descriptor %d, user %s saiu.\n", sala_id, sd, salas[sala_id].clientes[cliente_id].nome);
+    // retirar o descritor da master e da sala.
+    salas[sala_id].clientes[cliente_id].ativo = 0;
+    // Ao sair da sala, deve-se diminuir a quantidade de clientes
+    salas[sala_id].quantidade--;
+    if (retirar_master == 1)
+        FD_CLR(sd, &master);
+    FD_CLR(sd, &salas[sala_id].sala_fd);
+
+    // E caso a quantidade fique igual 0, deve-se fechar a mesma
+    // deixando-a inativa e desalocando o vetor de clientes
+    if (salas[sala_id].quantidade == 0) {
+        printf("Sala %d: sairam todos os users, sala sera desativada!\n", sala_id);
+        free(salas[sala_id].clientes);
+        salas[sala_id].ativo = false;
+    }
+}
+
+int cria_sala (int limite) {
+    // Para criar uma sala, deve-se encontrar a primeira sala
+    // vazia (ativo = 0) setar como ativa e atualizar seu limite
+    int sala;
+    for (sala = 0; sala < MAX_SALAS; sala++)
+        if (salas[sala].ativo == false)
+            break;
+
+    salas[sala].ativo = 1;
+    salas[sala].limite = limite;
+    salas[sala].clientes = malloc(limite * sizeof(cliente));
+
+    // Apos isso, deve-se instanciar o seu vetor de clientes
+    // e desativar todos os presentes. Tambem e necessario 
+    // retornar o valor da sala
+    for (int i = 0; i < limite; i++)
+        salas[sala].clientes[i].ativo = false;
+
+    printf("Sala %d: ativada, capacidade maxima de %d usuarios.\n", sala, limite);
+    return sala;
+}
 
 void envia_msg (int sd, int server_sd, int sala_id, int cliente_id) {
-    printf("Enviando mensagem do file descriptor %d na sala %d\n", sd, sala_id);
-    // Para cada file descriptor
+    printf("Sala %d: mensagem do file descriptor %d enviada.\n", sala_id, sd);
+    // Descriptor para cada file
     for (int j = 0; j <= fdmax; j++)
         // checa se ele esta no cesto do master
         if (FD_ISSET(j, &salas[sala_id].sala_fd))
@@ -45,63 +100,9 @@ void envia_msg (int sd, int server_sd, int sala_id, int cliente_id) {
             }
 }
 
-
-void sair_da_sala (int sd, int sala_id, int cliente_id, int retirar_master) {
-    printf("File descriptor %d saindo na sala %d\n", sd, sala_id);
-    // Ao sair da sala, deve-se diminuir a quantidade de clientes
-    // retirar o descritor da master e da sala.
-    salas[sala_id].clientes[cliente_id].ativo = 0;
-    salas[sala_id].quantidade--;
-    if (retirar_master == 1)
-        FD_CLR(sd, &master);
-    FD_CLR(sd, &salas[sala_id].sala_fd);
-
-    // E caso a quantidade fique igual 0, deve-se fechar a mesma
-    // deixando-a inativa e desalocando o vetor de clientes
-    if (salas[sala_id].quantidade == 0) {
-        free(salas[sala_id].clientes);
-        salas[sala_id].ativo = 0;
-    }
-}
-
-
-void inicia_servidor () {
-    // Inicializacao do servidor, zerando todas as salas
-    for (int i = 0; i < 100; i++) {
-        FD_ZERO(&salas[i].sala_fd);
-        salas[i].limite = 0;
-        salas[i].quantidade = 0;
-        salas[i].ativo = 0;
-    }
-}
-
-
-int cria_sala (int limite) {
-    // Para criar uma sala, deve-se encontrar a primeira sala
-    // vazia (ativo = 0) setar como ativa e atualizar seu limite
-    int sala;
-    for (sala = 0; sala < 100; sala++)
-        if (salas[sala].ativo == 0)
-            break;
-
-    salas[sala].ativo = 1;
-    salas[sala].limite = limite;
-    salas[sala].clientes = malloc(limite * sizeof(cliente));
-
-    // Apos isso, deve-se instanciar o seu vetor de clientes
-    // e desativar todos os presentes. Tambem e necessario 
-    // retornar o valor da sala
-    for (int i = 0; i < limite; i++)
-        salas[sala].clientes[i].ativo = 0;
-
-    printf("Nova sala %d criada\n", sala);
-    return sala;
-}
-
-
-void inserir_na_sala(int sd, int sala_id, char nome[], int tam_nome) {
+void entrar_na_sala(int sd, int sala_id, char nome[], int tam_nome) {
     
-    printf("File descriptor %d entrando na sala %d\n", sd, sala_id);
+    printf("Sala %d: file descriptor %d entrando.\n", sala_id, sd);
     // Para inserir na sala, deve-se aumentar a quantidade, adicionar
     // o descritor no cesto da sala, encontra uma posição na sala
     // que esteja vazia (cliente.ativo = 0) e insere seus atributos
@@ -113,7 +114,7 @@ void inserir_na_sala(int sd, int sala_id, char nome[], int tam_nome) {
     }
     else {
         for (int i = 0; i < salas[sala_id].limite; i++) {
-            if (salas[sala_id].clientes[i].ativo == 0) {
+            if (salas[sala_id].clientes[i].ativo == false) {
                 salas[sala_id].clientes[i].cliente_sd = sd;
                 salas[sala_id].clientes[i].ativo = 1;
                 strncpy(salas[sala_id].clientes[i].nome, nome, tam_nome);
@@ -123,15 +124,14 @@ void inserir_na_sala(int sd, int sala_id, char nome[], int tam_nome) {
     }
 }
 
-
 void executa_comando (int sd, int sala_id, int cliente_id) {
     buf[strlen(buf) - 2] = '\0';
     printf("Comando \"%s\" acionado na sala %d pelo file descriptor %d\n", buf, sd, sala_id);
-    char resp_buf[256];
+    char resp_buf[MAX_STR_SIZE];
 
     // Se o recv retornar 0 ou a mensagem foi de sair
     // retira o socket descriptor do cesto
-    if (strncmp(buf+1, "sair", 4) == 0) {
+    if (strncmp(buf+1, "sair", 4) == 0 ) {
         printf("Desconectando descritor %d\n", sd);
         strcpy(resp_buf, "Cliente Desconectado\n");
         send(sd, resp_buf, strlen(resp_buf), 0);
@@ -163,24 +163,22 @@ void executa_comando (int sd, int sala_id, int cliente_id) {
     // Caso o cliente queira trocar de sala, deve-se executar duas
     // rotinas, a de sair de uma sala e a de inserir em um sala
     if (strncmp(buf+1, "trocar_sala", 11) == 0) {
-        recv(sd, buf, 256, 0);
+        recv(sd, buf, MAX_STR_SIZE, 0);
         int nova_sala = atoi(buf);
-        char nome[256];
+        char nome[MAX_STR_SIZE];
         strcpy(nome, salas[sala_id].clientes[cliente_id].nome);
         sair_da_sala(sd, sala_id, cliente_id, 0);
-        inserir_na_sala(sd, nova_sala, nome, strlen(nome));
+        entrar_na_sala(sd, nova_sala, nome, strlen(nome));
     }
 }
 
-
 int main (int argc, char *argv[]) {
-    
     if (argc < 3) {
-        printf("Digite IP e porta para o servidor\n");
+        printf("Digite IP e porta para o servidor\nExemplo: 127.0.0.1 4000\n");
         exit(1);
     }
 
-    // Limpeza dos sets master e das salas
+    // Faz a Limpeza dos sets master e das salas e inicializa o servidor
     FD_ZERO(&master);
     FD_ZERO(&read_fds);
     inicia_servidor();
@@ -225,21 +223,21 @@ int main (int argc, char *argv[]) {
 
                     // Recebe nome do usuario e sala que quer entrar
                     int limite, tam_nome;
-                    char nome[256];
-                    tam_nome = recv(newfd, nome, 256, 0);
+                    char nome[MAX_STR_SIZE];
+                    tam_nome = recv(newfd, nome, MAX_STR_SIZE, 0);
                     tam_nome -= 2;
-                    recv(newfd, buf, 256, 0);
+                    recv(newfd, buf, MAX_STR_SIZE, 0);
                     sala = atoi(buf);
                     
                     // Se a sala for -1, cria uma nova com o limite informado
                     if (sala == -1) {
-                        recv(newfd, buf, 256, 0);
+                        recv(newfd, buf, MAX_STR_SIZE, 0);
                         limite = atoi(buf);
                         sala = cria_sala(limite);
                     }
 
                     // De qualquer forma insere ele na sala nova ou existente
-                    inserir_na_sala(newfd, sala, nome, tam_nome);
+                    entrar_na_sala(newfd, sala, nome, tam_nome);
 
                     // Se o valor do sd for maior que o atual (mais coisas no cesto)
                     // atualiza esse valor para as proximas iteracoes do loop
@@ -254,7 +252,7 @@ int main (int argc, char *argv[]) {
 
                     // Encontra a sala que o descritor do socker se encontra
                     int sala_id;
-                    for (sala_id = 0; sala_id < 100; sala_id++)
+                    for (sala_id = 0; sala_id < MAX_SALAS; sala_id++)
                         if (FD_ISSET(i, &salas[sala_id].sala_fd))
                             break;
 
